9e608fcd8149d96e0390716be535ff262c68160c
anastasia-yildirim <anastasia.skrydlova@gmail.com>
2024-07-22 17:01:42 +0300

    Added a test without actions() to HerokuAppTests

--- src/test/java/tests/selenide2_hw/HerokuAppTests.java
+++ src/test/java/tests/selenide2_hw/HerokuAppTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class HerokuAppTests {
@@ -19,12 +20,36 @@ public class HerokuAppTests {
     }
 
     @Test
+    public void herokuAppDragAndDropStepByStepActionsTest() {
+        //Act
+        open("/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+        //Assert
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
     public void herokuAppDragAndDropTest() {
         //Act
         open("/drag_and_drop");
         $("#column-a").shouldHave(text("A"));
         $("#column-b").shouldHave(text("B"));
-        actions().dragAndDrop($("#column-a"), $("#column-b")).perform();
+        $("#column-a").dragAndDrop(to($("#column-b")));     //работает
+        //Assert
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    public void herokuAppDragAndDropActionsSecondTest() {
+        //Act
+        open("/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        actions().dragAndDrop($("#column-a"), $("#column-b")).perform();    // и так работает
         //Assert
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
